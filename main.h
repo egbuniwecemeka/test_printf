96eec3701a9d0192c9823550e01699747db21b29
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-09-18 00:34:22 -0700

    headers

--- main.h
+++ main.h
@@ -2,13 +2,120 @@
 #define MAIN_H
 
 /*Libraries*/
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h> /*For */
+
+#include <unistd.h>  /*for display the function write */
+
+#include <stdarg.h>   /*for  */
+
+#include <stdarg.h>   /*for argument after "..."*/  
+
+#include <limits.h>   /*for max byet no. */  
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**** .   FOR THE TASKS   . ****/
 /*0*/
 int _printf(const char *format, ...);
+
+
+
+
+
+
+
+/**
+ * struct specifier - struc out
+ * 
+*/
+typedef struct specifier
+{
+	/* data */
+	char *specifier; /*string / 1 char*/
+}; specifier_t /*chang char to specifier type*/
+
+/*display*/
+/* puts 1st brings whole string for putchar */
+/* then putchar pasifier each single char*/
+
 /*1*/
+
+/*2*/
+
+/*3*/
+
+
+/*4*/
+
+#define OUT_PUT_BUFF_SIZE 1024
+#define BUFF_FLUSH -1
+
+/*5*/
+
+/*6*/
+
+
+#define PARAMS_INT {0, 0, 0, 0, 0,} . /* X10 */
+/**
+ * struct parameters - params struc
+ * @
+ * @unsign: unsign value
+ * 
+*/
+typedef struct parameters 
+{
+	/* tasks functions */
+/* F z user input is = 2 one of these turn 0 to 1 */
+/*7*/
+
+/*8*/
+
+/*7*/
+
+/*8*/
+
+/*9*/
+
+/*10*/
+
+/*11*/
+
+/*12*/
+
+}; params_t  /*to change z name 2 params type*/
+/* use < ptams_t prams = PARAMS_INT > */
+
+
+
+
+
+/*13* Reversed */
+
+
+/*14*/
+
+
+
+/*15*/
+
+
+
+/*16*/
+
+
+
+
 
 #endif
